f34d4d5576b6d4ca6d56323499f7a44576a666ef
ajit123jain <ajitkumarjain123@gmail.com>
2021-06-10 18:17:18 +0530

    authentication fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,18 @@
 class ApplicationController < ActionController::API
 
-	def authenticate_api_v1_user!(options = {})
-		byebug 
-		if user_signed_in? 
+	def authenticate_api_v1_user!(options = {})  
+		if api_v1_user_signed_in? 
 			super(options)
 		else
 			render json: { error: "You are not logged in." }, status: :unauthorized
 		end 	
 	end
 
-	# def authenticate_admin!(options = {})
-	# 	if admin_signed_in? 
-	# 		super(options)
-	# 	else
-	# 		render json: { error: "You are not logged in." }, status: :unauthorized
-	# 	end 	
-	# end
+	def authenticate_admin!(options = {})
+		if api_v1_admin_signed_in? 
+			super(options)
+		else
+			render json: { error: "You are not logged in." }, status: :unauthorized
+		end 	
+	end
 end
