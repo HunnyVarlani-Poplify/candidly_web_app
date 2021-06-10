ffec7e0263036ceca5f89f38981e43fe066b3fbb
ajit123jain <ajitkumarjain123@gmail.com>
2021-06-10 16:33:39 +0530

    domain reserved validation

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,5 +1,7 @@
 class Tenant < ApplicationRecord
   validates_uniqueness_of :subdomain
+  validates :subdomain, exclusion: { in: %w( www admin test public private staging app web demo net ),
+    message: "%{value} is reserved." }
 
   has_one :company
 end
