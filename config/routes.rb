7eaa4d608746eaa58a7a5d0ff5b6e73a36f707f9
HunnyVarlani-Poplify <hunny@poplify.com>
2021-06-14 16:38:47 +0530

    Edit password url mapped

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   namespace :api  do
     namespace :v1 do 
-      devise_for :users, defaults: { format: :json }, controllers: {sessions: 'api/v1/users/sessions', registrations: 'api/v1/users/registrations', passwords: 'api/v1/users/passwords'}
+      devise_for :users, defaults: { format: :json }, controllers: {sessions: 'api/v1/users/sessions', registrations: 'api/v1/users/registrations', passwords: 'api/v1/users/passwords' }
       devise_for :admins, defaults: { format: :json }, controllers: { sessions: 'api/v1/admins/sessions' }
 
       resources :companies, format: 'json'
       post '/sign_in'	=>  'home#sign_in_user', as: :sign_in_user
       get '/list_of_domains' => 'home#list_of_domains', as: :list_of_domains
     end
+  end
+  constraints subdomain: 'app.demo' do
+    get 'reset_password', to: 'api/v1/users/passwords#edit' , as: :edit_password
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
